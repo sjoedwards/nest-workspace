79b736151a638feaf4ba65cabd08e948a23de3bd
sjoedwards <samuel.j.o.edwards@gmail.com>
2022-08-18 20:13:18 +0100

    mono-repo

--- 4-mycarvalue/src/app.module.ts
+++ 4-mycarvalue/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsService } from './reports/reports.service';
+import { ReportsController } from './reports/reports.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [AppController, ReportsController],
+  providers: [AppService, ReportsService],
 })
 export class AppModule {}
